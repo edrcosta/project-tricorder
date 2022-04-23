52480006776dfed6c59fff894e3c3a64d76995a7
Eder Costa <eder@encantos.co>
2022-04-23 00:00:36 -0300

    The jorney continues - update computer architecture

--- README.md
+++ README.md
@@ -9,38 +9,13 @@ Build a modular handhelt 8 bit retro computer "with a power of an gameboy".
 
 ## the computer 
 
-### Prototype 
+The current version its only a BIOS system to host z80 processors
 
-![image](https://user-images.githubusercontent.com/3594012/129946228-5372357b-6732-480a-ae3c-3ba592b1518f.png)
-![image](https://user-images.githubusercontent.com/3594012/129946258-13c03898-69d1-4f19-8981-056742ff2baf.png)
+the core: 1 atmega328p that host and run the BIOS system
+display: TFT display
+input: cardKb its a qwerty i2c keyboard module
 
+## What im researching 
 
-### Architecture
-
-There will be 4 components at this computer a z80a processor, a atmega328p as chip set capable of SPI video output and bootloading, a atmega328p to be a sound generator IC, and a AT89S51 (8051) as a north bridge to control data flow into and out of the z80a processor... and another 8051 to control a plug and play expansion port 
-
-At firt i will attempt to create an basic BIOS program to load assembly code direct into the z80... that will be stored into the atmega328p (chipset)... and a simple firmware into both of the 8051 to comunicate and receive commands from the chipset... 
-
-### Components 
-
-1. 2x - Atmega328p 
-2. 2x - 8051 
-3. 1x - z80a
-
-Schematics (in progress): 
-
-![image](https://user-images.githubusercontent.com/3594012/128116066-58a8413d-4160-4418-8acc-8078fc7dd635.png)
-
-Block diagram: 
-
-![image](https://user-images.githubusercontent.com/3594012/128116848-bf59ea1c-448e-4fd6-adf5-2a03c9dc9903.png)
-
-https://en.wikipedia.org/wiki/Southbridge_(computing)
-https://en.wikipedia.org/wiki/Northbridge_(computing)
-https://en.wikipedia.org/wiki/Chipset
-https://en.wikipedia.org/wiki/BIOS
-
-## Related repositories 
-
-atmega328p testing code: https://github.com/edrcosta/projects.arduino 
-8051 testing code: https://github.com/edrcosta/8051-mcu-tests
+Learning I2c 
+Learning about data buses 
--- src/chipset.ino
+++ src/chipset.ino
@@ -1,169 +1,78 @@
-// DISPLAY ####################################################################
+#include <Wire.h>
 #include <TFT.h>  
 #include <SPI.h>
 
-// TFT DISPLAY
-#define DISPLAY_RESET_INPUT 8
-#define DISPLAY_CS_INPUT 10
-#define DISPLAY_DS_INPUT 9
-TFT TFTscreen = TFT(DISPLAY_CS_INPUT, DISPLAY_DS_INPUT, DISPLAY_RESET_INPUT);
+// TFT 
+#define cs   10
+#define dc   9
+#define rst  8
 
-// Rotary ####################################################################
-// Rotary Encoder Inputs
-#define ROTARY_ENCODERT_CLK_INPUT 2
-#define ROTARY_ENCODERT_DT 3
-#define ROTARY_ENCODERT_SW 4
+TFT TFTscreen = TFT(cs, dc, rst);
 
-// rotary encoder states
-int ROTARY_COUNTER = 0;
-int ROTARY_ENCODER_CURRENT_STATE_CLK;
-int ROTARY_ENCODER_LAST_STATE_CLK;
-int ROTARY_ENCODER_HAS_CHANGED = 0;
-String ROTARY_ENCODER_DIRECTION ="";
-unsigned long ROTARY_ENCODER_BUTTON_LAST_STATE = 0;
+// i2c keyboard cardkb
+int KeyboardI2CAddress = 0x5F;
+int X0,X1,X_out;
 
-// JOYSTICK ####################################################################
-/* Pins */
-#define JOYSTICK_INPUT_X A1
-#define JOYSTICK_INPUT_Y A0
-#define JOYSTICK_INPUT_BUTTON A2
+// address DATAX0 register
+#define X_Axis_Register_DATAX0 0x32
+// address DATAX1 register
+#define X_Axis_Register_DATAX1 0x33
+// Power Register
+#define Power_Register 0x2D
 
-/* Joystick information */
-int JOYSTICK_X = 0 ;
-int JOYSTICK_Y = 0 ; 
-int JOYSTICK_BUTTON = 0 ;
-int JOYSTICK_PREVIOUS_X = 0;
-int JOYSTICK_PREVIOUS_Y = 0;
-int JOYSTICK_DIRECTION_HAS_CHANGED = 1;
 
-/*
-  Check rotary encoder current state
-*/
-void rotary_encoder_get_status(){
-  ROTARY_ENCODER_CURRENT_STATE_CLK = digitalRead(ROTARY_ENCODERT_CLK_INPUT);
+char keyboardBuffer[] = "";
 
-  // CHECK IF STATE CHANGE 
-  if (ROTARY_ENCODER_CURRENT_STATE_CLK != ROTARY_ENCODER_LAST_STATE_CLK  && ROTARY_ENCODER_CURRENT_STATE_CLK == 1){    
-    if (digitalRead(ROTARY_ENCODERT_DT) != ROTARY_ENCODER_CURRENT_STATE_CLK) {
-      // DECREMENT
-      ROTARY_COUNTER --;
-      ROTARY_ENCODER_DIRECTION ="DEC";
-    } else {
-      // INCREMENT
-      ROTARY_COUNTER ++;
-      ROTARY_ENCODER_DIRECTION ="INC";
-    }
-    ROTARY_ENCODER_HAS_CHANGED = 1;
-  }
 
-  // STORE STATE
-  ROTARY_ENCODER_LAST_STATE_CLK = ROTARY_ENCODER_CURRENT_STATE_CLK;
-
-  // ROTARY ENCODER BUTTON CHECK
-  if (digitalRead(ROTARY_ENCODERT_SW) == LOW) {
-    if (millis() - ROTARY_ENCODER_BUTTON_LAST_STATE > 50) {
-      Serial.println("Button pressed!");
-    }
-    ROTARY_ENCODER_BUTTON_LAST_STATE = millis();
-  }
+void enableReadingI2c(){
+  Wire.beginTransmission(KeyboardI2CAddress);
+  Wire.write(Power_Register);
+  // Bit D3 High for measuring enable (0000 1000)
+  Wire.write(8);  
+  Wire.endTransmission();
 }
 
-void joystick_get_status(){
-    JOYSTICK_X = analogRead(JOYSTICK_INPUT_X);  
-    JOYSTICK_Y = analogRead(JOYSTICK_INPUT_Y);  
-    JOYSTICK_BUTTON = digitalRead(A2);
-
-    if(JOYSTICK_DIRECTION_HAS_CHANGED == 1){
-        JOYSTICK_PREVIOUS_X = JOYSTICK_X;
-        JOYSTICK_PREVIOUS_Y = JOYSTICK_Y;
-        JOYSTICK_DIRECTION_HAS_CHANGED = 0;
-    }
-
-    if(JOYSTICK_X > JOYSTICK_PREVIOUS_X){
-        if (JOYSTICK_X - JOYSTICK_PREVIOUS_X > 5){
-            JOYSTICK_DIRECTION_HAS_CHANGED = 1;
-        }
-    }else{
-        if (JOYSTICK_PREVIOUS_X - JOYSTICK_X > 5){
-            JOYSTICK_DIRECTION_HAS_CHANGED = 1;
-        }
-    }
-
-    if(JOYSTICK_Y > JOYSTICK_PREVIOUS_Y){
-        if (JOYSTICK_Y - JOYSTICK_PREVIOUS_Y > 5){
-            JOYSTICK_DIRECTION_HAS_CHANGED = 1;
-        }
-    }else{
-        if (JOYSTICK_PREVIOUS_Y - JOYSTICK_Y > 5){
-            JOYSTICK_DIRECTION_HAS_CHANGED = 1;
-        }
-    }
+void resetDisplay() {
+  TFTscreen.begin();
+  TFTscreen.background(0, 0, 0);
 }
 
 void setup() {
   Serial.begin(9600);
-
-  // DISPLAY ########################################
-  TFTscreen.begin();
-  TFTscreen.setTextSize(1);
-  TFTscreen.background(0, 0, 0);
-
-  // ROTARY ########################################
-  pinMode(ROTARY_ENCODERT_CLK_INPUT,INPUT);
-  pinMode(ROTARY_ENCODERT_DT,INPUT);
-  pinMode(ROTARY_ENCODERT_SW, INPUT_PULLUP);
-  ROTARY_ENCODER_LAST_STATE_CLK = digitalRead(ROTARY_ENCODERT_CLK_INPUT);
-
-  // JOYSTICK ########################################
-  pinMode(JOYSTICK_INPUT_BUTTON, INPUT_PULLUP);
+  Wire.begin();
+  delay(100);
+  enableReadingI2c();
+  resetDisplay();
 }
 
-int UPDATE_SCREEEN_COUNT = 0;
- 
+void readKeyboard(){
+  // Init connection
+  Wire.beginTransmission(KeyboardI2CAddress);
+  // Read data registers
+  Wire.write(X_Axis_Register_DATAX0);
+  Wire.write(X_Axis_Register_DATAX1);
+  Wire.endTransmission();
+  // Request
+  Wire.requestFrom(KeyboardI2CAddress, 2);
+  // Response
+  if(Wire.available()<=2) {  
+    // Reads registers
+    X0 = Wire.read();
+    X1 = Wire.read();   
+  }
+
+  if(X0 > 0){
+    keyboardBuffer = strcat(keyboardBuffer, (char)X0);
+    Serial.print("X0= ");
+    Serial.print((char)X0);
+    Serial.print(keyboardBuffer);
+    Serial.print("   X1= ");
+    Serial.println(X1);
+  }
+}
+
 void loop() {
-    // joystick_get_status();
-    // rotary_encoder_get_status();
-
-    // // JOYSTICK DEBUG #################
-    // if(JOYSTICK_DIRECTION_HAS_CHANGED){
-    //     /*Joystick STATUS*/
-    //     Serial.print(" X:");
-    //     Serial.print(JOYSTICK_X, DEC);
-    //     Serial.print(" Y:");
-    //     Serial.print(JOYSTICK_Y, DEC);  
-       
-    //     Serial.print("\n");
-    //     JOYSTICK_DIRECTION_HAS_CHANGED = 0;
-    // }
-
-    // if(JOYSTICK_BUTTON == 0){
-    //     Serial.print(" B:");
-    //     Serial.println(JOYSTICK_BUTTON);
-    //     Serial.print("\n");
-    // }
-    
-    // // DEBUG ENCODER
-    // if(ROTARY_ENCODER_HAS_CHANGED == 1){
-    //   Serial.print("Direction: ");
-    //   Serial.print(ROTARY_ENCODER_DIRECTION);
-    //   Serial.print(" | ROTARY_COUNTER: ");
-    //   Serial.println(ROTARY_COUNTER);
-    //   ROTARY_ENCODER_HAS_CHANGED = 0;
-    // }
-
-
-
-    if(UPDATE_SCREEEN_COUNT == 10){
-      TFTscreen.stroke(random(0, 255), random (0, 255), random (0, 255));
-      TFTscreen.text("Hello World", 2, 10);
-
-      TFTscreen.text("Hello World", 2, 30);
-
-      TFTscreen.text("Hello World", 2, 50);
-      UPDATE_SCREEEN_COUNT = 0
-    }
-
-    UPDATE_SCREEEN_COUNT++;
-
-    delay(10);
+  readKeyboard();
 }
+
+
